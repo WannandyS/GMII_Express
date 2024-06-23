cd9e8b6716b069e0bf729ea702150885f26cfafe
WannandyS <161842255+WannandyS@users.noreply.github.com>
2024-06-23 08:52:49 +0700

    Update

---
+++ public/GMII/assets/javascripts/style.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      plugins: [ 'interaction', 'dayGrid', 'timeGrid' ],
+      defaultView: 'dayGridMonth',
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+      },
+    });
+    calendar.render();
+  });
