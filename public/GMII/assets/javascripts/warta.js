da1994cb2fdff853a25c285d6c63cd07eec5d1a9
WannandyS <161842255+WannandyS@users.noreply.github.com>
2024-06-24 14:03:41 +0700

    Update warta.js

--- public/GMII/assets/javascripts/warta.js
+++ public/GMII/assets/javascripts/warta.js
@@ -1,5 +1,5 @@
 console.log ('text');
-fetch("https://gmii.gajiin.my.id/warta/fe").then((res) => res.json()).then((res) => {
+fetch("https://gmii.gajiin.my.id/warta").then((res) => res.json()).then((res) => {
     for(const row of res){
     console.log(res);
     const warta = document.createElement("div");
